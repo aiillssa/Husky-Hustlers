9e66cf7f05c4d53f4afff3cae4e9068821aa4c1c
Katharine Zhang <kyiranzhang@gmail.com>
2024-10-19 22:04:42 -0700

    made some changes to App, but a bit lost as to how to display LoginPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <div className="App">It's displaying here, but I don't know how to get it to display login</div>
   );
 }
 
