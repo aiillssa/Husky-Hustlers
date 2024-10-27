6a091efcc422d79c580fc0cc40def5fb7eed5b15
Kevin <Kev.mtran001@gmail.com>
2024-10-26 17:48:42 -0700

    Login Page is now rendered with App routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/NavBar';
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom"
 import LoginPage from './pages/LoginPage/LoginPage';
 import Homepage from './pages/Homepage/Homepage';
 import Profile from './pages/Profile';
 
-function App() {
-  return (
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='/pages/LoginPage/LoginPage' element={<LoginPage />} />
-          <Route path='/pages/Homepage/Homepage' element={<Homepage />} />
-          <Route path='/pages/Profile' element={<Profile />}/>
-        </Routes>
-      </BrowserRouter>
-  );
+type AppState = {
+  signedIn: boolean;
+};
+
+export class App extends Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { signedIn: false };
+  }
+
+  setSignedIn = (value: boolean) => {
+    this.setState({ signedIn: value });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.signedIn ? (
+          <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route path="/pages/Profile" element={<Profile />} />
+              <Route path="/pages/Homepage" element={<Homepage />} />
+            </Routes>
+          </BrowserRouter>
+        ) : (
+          <LoginPage setSignedIn={this.setSignedIn} /> // Pass setSignedIn to LoginPage
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,19 +1,12 @@
-import { memo } from "react";
-import type { FC } from "react";
-
+import React, { useState, useEffect } from "react";
+import { memo, FC } from "react";
+import { googleLogout, useGoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+import axios from "axios";
 import classes from "./LoginPage.module.css";
 import resets from "../../components/_resets.module.css";
 
-import React, { useState, useEffect } from "react";
-import {
-  googleLogout,
-  useGoogleLogin,
-  GoogleOAuthProvider,
-} from "@react-oauth/google";
-import axios from "axios";
-
 interface Props {
-  className?: string;
+  setSignedIn: (value: boolean) => void; // Accepts setSignedIn as a prop from App.tsx
 }
 
 interface User {
@@ -27,10 +20,10 @@ interface Profile {
   hd?: string;
 }
 
-const App: React.FC = () => {
+const LoginPageContent: FC<Props> = ({ setSignedIn }) => {
   const [user, setUser] = useState<User | null>(null);
   const [profile, setProfile] = useState<Profile | null>(null);
-  const [errorMessage, setErrorMessage] = useState<string | null>(null); // State for error message
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   const login = useGoogleLogin({
     onSuccess: (codeResponse: User) => setUser(codeResponse),
@@ -41,11 +34,9 @@ const App: React.FC = () => {
     if (user) {
       axios
         .get(
-          //getting user data from google server
           `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
           {
             headers: {
-              // allmighty user token
               Authorization: `Bearer ${user.access_token}`,
               Accept: "application/json",
             },
@@ -53,42 +44,36 @@ const App: React.FC = () => {
         )
         .then((res) => {
           const userProfile: Profile = res.data;
-          // Check if hd exists and is equal to 'uw.edu'
           if (userProfile.hd === "uw.edu") {
             setProfile(userProfile);
-            setErrorMessage(null); // Clear any previous errors
-            console.log("set profile");
+            setErrorMessage(null);
+            setSignedIn(true); // Update signedIn state in App.tsx
           } else {
-            setProfile(null); // Clear profile if hd is not uw.edu
-            setErrorMessage("Access denied. Please use a UW email account."); // Set error message
+            setProfile(null);
+            setErrorMessage("Access denied. Please use a UW email account.");
           }
         })
         .catch((err) => console.log(err));
     }
-  }, [user]);
+  }, [user, setSignedIn]);
 
-  // log out button
   const logOut = () => {
     googleLogout();
     setProfile(null);
-    setErrorMessage(null); // Clear error message on logout
+    setErrorMessage(null);
+    setSignedIn(false); // Reset signedIn on logout
   };
 
   return (
     <div>
-      <h2>React Google Login</h2>
-      <br />
-      <br />
-      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}{" "}
-      {/* Render error message if it exists */}
+      <h2>Login Here!</h2>
+      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
       {profile ? (
         <div>
           <img src={profile.picture} alt="user" />
           <h3>User Logged in</h3>
           <p>Name: {profile.name}</p>
           <p>Email Address: {profile.email}</p>
-          <br />
-          <br />
           <button onClick={logOut}>Log out</button>
         </div>
       ) : (
@@ -98,15 +83,12 @@ const App: React.FC = () => {
   );
 };
 
-// Must wrap the page in the OAuth Provider to appease master google
-export const LoginPage: FC<Props> = memo(function LoginPage(props = {}) {
+export const LoginPage: FC<Props> = memo(function LoginPage(props: Props) {
   return (
     <div className={`${resets.clapyResets} ${classes.root}`}>
-      <React.StrictMode>
-        <GoogleOAuthProvider clientId="410136854211-33d088kspbh9se3oej3sebpmj0jal8v7.apps.googleusercontent.com">
-          <App />
-        </GoogleOAuthProvider>
-      </React.StrictMode>
+      <GoogleOAuthProvider clientId="410136854211-33d088kspbh9se3oej3sebpmj0jal8v7.apps.googleusercontent.com">
+        <LoginPageContent {...props} />
+      </GoogleOAuthProvider>
     </div>
   );
 });
