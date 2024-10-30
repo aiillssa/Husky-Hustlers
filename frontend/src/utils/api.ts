c4fa9e76c9c69dfebccd8a29edb81ad8ae6b269e
Aaditya Thukral <aadityathukral2003@gmail.com>
2024-10-29 20:42:56 -0700

    bug fixed cors

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,22 +1,23 @@
 export const addUser = async (name: string, email: string): Promise<void> => {
-    try {
-        const response = await fetch('http://localhost:8088/users', {
-            mode: 'no-cors',
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ 
-                key1: name, 
-                key2: email 
-            }),
-        });
+  try {
+    const urlencodedBody = new URLSearchParams();
+    urlencodedBody.append("name", name);
+    urlencodedBody.append("email", email);
+    console.log(name, email);
+    const response = await fetch("http://localhost:8088/users", {
+      //   mode: "no-cors",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: name, email: email }),
+    });
 
-        const data = await response.json();
-        console.log(data); // Check if the backend responds correctly
-    } catch (error) {
-        console.error('Error connecting to backend:', error);
-    }
+    const data = await response.json();
+    console.log(`This is the data: ${data}`); // Check if the backend responds correctly
+  } catch (error) {
+    console.error("Error connecting to backend:", error);
+  }
 };
 
-export{};
+export {};
