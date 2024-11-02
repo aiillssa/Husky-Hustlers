ee2ddc867cda36be36226944bec093c4eb8f9b3e
yuxinfu <yuxinfu@uw.edu>
2024-11-02 16:00:21 -0700

    fixing login page layout

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect, FC } from "react";
 import { memo } from "react";
-import { googleLogout, useGoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+import {
+  googleLogout,
+  useGoogleLogin,
+  GoogleOAuthProvider,
+} from "@react-oauth/google";
 import axios from "axios";
 import classes from "./LoginPage.module.css";
 import resets from "../../components/_resets.module.css";
@@ -65,36 +69,47 @@ const LoginPageContent: FC<Props> = ({ setSignedIn }) => {
   };
 
   return (
-    <div className={`${resets.clapyResets} ${classes.root}`}>
-      <div className={classes.caption}>A platform that fosters student businesses and UW community!</div>
-      <div className={classes.loginComponent}>
-        <div className={classes.frame1}>
-          <div className={classes.formLogIn}>
-            {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-            {profile ? (
-              <div>
-                <img src={profile.picture} alt="user" />
-                <h3>User Logged in</h3>
-                <p>Name: {profile.name}</p>
-                <p>Email Address: {profile.email}</p>
-                <button onClick={logOut}>Log out</button>
-              </div>
-            ) : (
-              <button onClick={() => login()}>Sign in with Google 🚀</button>
-            )}
-          </div>
-        </div>
-        <div className={classes.title}>
-          <div className={classes.helloWelcome}>Hello, Welcome!</div>
-          <div className={classes.logInPage}>Log-In Page</div>
-        </div>
-      </div>
-      <div className={classes.image2}></div>
-      <div className={classes.uWLogo}></div>
+    <div className={`${classes.root}`}>
       <div className={classes.navBar}>
         <div className={classes.huskyHustler}>Husky Hustler</div>
-        <div className={classes.logInSignUpFirstToSeeAllStuden}>Log In / Sign Up First to See All Student Business</div>
+        <div className={classes.logInSignUpFirstToSeeAllStuden}>
+          Log In / Sign Up First to See All Student Business
+        </div>
       </div>
+      <div className={classes.content_wrapper}>
+        <div className={classes.caption}>
+          A platform that fosters student businesses and UW community!
+        </div>
+        <div className={classes.loginComponent}>
+          <div className={classes.title}>
+            <div className={classes.helloWelcome}>Hello, Welcome!</div>
+            <div className={classes.logInPage}>Log-In Page</div>
+          </div>
+          <div className={classes.frame1}>
+            <div className={classes.formLogIn}>
+              {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+              {profile ? (
+                <div>
+                  <img src={profile.picture} alt="user" />
+                  <h3>User Logged in</h3>
+                  <p>Name: {profile.name}</p>
+                  <p>Email Address: {profile.email}</p>
+                  <button onClick={logOut}>Log out</button>
+                </div>
+              ) : (
+                <button
+                  className={classes.login_button}
+                  onClick={() => login()}
+                >
+                  Sign in with Google 🚀
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   );
 };
