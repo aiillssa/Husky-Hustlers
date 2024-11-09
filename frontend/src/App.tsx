3c0666161d621c82e41dfc99da3e144715857d5c
Kevin <Kev.mtran001@gmail.com>
2024-11-09 14:06:24 -0800

    Changed rendering so AddBusinessPage does not render navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
 import Navbar from './components/NavBar';
 import LoginPage from './pages/LoginPage/LoginPage';
 import Homepage from './pages/Homepage/Homepage';
@@ -22,17 +22,30 @@ export class App extends Component<{}, AppState> {
     this.setState({ signedIn: value });
   };
 
+  // Layout component that includes Navbar
+  NavbarLayout = () => (
+    <>
+      <Navbar />
+      <Outlet /> {/* This renders the nested route's component */}
+    </>
+  );
+
   render() {
     return (
       <div>
         {this.state.signedIn ? (
           <BrowserRouter>
-            <Navbar />
             <Routes>
-              <Route path="/pages/Profile" element={<Profile />} />
-              <Route path="/pages/Homepage" element={<Homepage />} />
-              <Route path="/pages/BusinessPage" element={<BusinessPage />} />
+              {/* Routes with Navbar */}
+              <Route element={<this.NavbarLayout />}>
+                <Route path="/pages/Profile" element={<Profile />} />
+                <Route path="/pages/Homepage" element={<Homepage />} />
+                <Route path="/pages/BusinessPage" element={<BusinessPage />} />
+              </Route>
+
+              {/* Routes without Navbar */}
               <Route path="/pages/AddBusinessPage" element={<AddBusinessPage />} />
+
               {/* Redirect to Homepage after login */}
               <Route path="/" element={<Navigate to="/pages/Homepage" replace />} />
             </Routes>
