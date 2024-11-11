477947ad598b1018528d3adc335bc54d239f3cf7
Katharine Zhang <kyiranzhang@gmail.com>
2024-11-11 11:18:54 -0800

    added dummy tests in src/Tests to test CI/CD

---
+++ frontend/src/Tests/dummyTest.tsx
@@ -0,0 +1,14 @@
+// SumComponent.test.js
+import { render, screen } from '@testing-library/react';
+import DummyComponent from './dummyTestComponent';
+
+test('displays 1 + 1 = 2', () => {
+  // Render the component
+  render(<DummyComponent />);
+
+  // Find the element that displays the result
+  const resultElement = screen.getByText('2');
+
+  // Assert that the result is correct
+  expect(resultElement).toBeInTheDocument();
+});
---
+++ frontend/src/Tests/dummyTestComponent.tsx
@@ -0,0 +1,9 @@
+// SumComponent.js
+import React from 'react';
+
+const DummyComponent = () => {
+  const sum = 1 + 1;
+  return <div>{sum}</div>;
+};
+
+export default DummyComponent;
