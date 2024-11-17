261730badd5e4a3596d09f0a270877730f8a8a2e
Kevin <Kev.mtran001@gmail.com>
2024-11-17 02:42:37 -0800

    Removed SearchBar CSS tsx: IE Hamburger Icon & Magnify Glass. Will reincorporate tomorrow.

--- frontend/src/components/HomePageC/SearchBar/SearchBar.tsx
+++ frontend/src/components/HomePageC/SearchBar/SearchBar.tsx
@@ -1,12 +1,6 @@
 import React, { memo } from 'react';
 import type { FC } from 'react';
 
-import resets from '../../_resets.module.css';
-import { IconButton } from '../IconButton/IconButton';
-import { Menu } from '../Menu/Menu';
-import { Search } from '../Search/Search';
-import { Icon } from './Icon';
-import { Icon2 } from './Icon2';
 import classes from './SearchBar.module.css';
 
 interface Props {
@@ -23,17 +17,6 @@ export const SearchBar: FC<Props> = memo(function SearchBar(props) {
   return (
     <div className={`${props.classes?.root || ''} ${props.className || ''} ${classes.root}`}>
       <div className={classes.stateLayer}>
-        <IconButton
-          swap={{
-            icon: (
-              <Menu
-                swap={{
-                  icon: <Icon className={classes.icon} />,
-                }}
-              />
-            ),
-          }}
-        />
         <div className={classes.content}>
           <input
             placeholder="  Type to search by business name..."
@@ -42,17 +25,6 @@ export const SearchBar: FC<Props> = memo(function SearchBar(props) {
           />
         </div>
         <div className={classes.trailingElements}>
-          <IconButton
-            swap={{
-              icon: (
-                <Search
-                  swap={{
-                    icon: <Icon2 className={classes.icon2} />,
-                  }}
-                />
-              ),
-            }}
-          />
         </div>
       </div>
     </div>
