a5be729180db6ee5a6fb6d27b6528533ca717b1c
Katharine Zhang <kyiranzhang@gmail.com>
2024-12-05 22:09:22 -0800

    singular image upload frontend with formData holding all parameters

--- frontend/src/components/uploadImageFormC/uploadImageForm.tsx
+++ frontend/src/components/uploadImageFormC/uploadImageForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+
 // https://www.youtube.com/watch?v=ijx0Uqlo3NA << to transition to multiple file uploads at once later
 
 interface DataToSend {
@@ -8,46 +9,42 @@ interface DataToSend {
     source: string;
   }
 const UploadImageForm = () => {
+    //const fs = require('fs');
+
     const[ file, setFile ] = useState<File | null>(null);
     const[ progress, setProgress ] = useState({started: false, prcnt: 0});
     const[ msg, setMsg ] = useState("");
-
-
+    
     function handleUpload() {
         if(!file) {
             setMsg("no file selected");
             return;
         }
+        
+        
         const formData = new FormData();
-        formData.append(file.name, (file));
+        formData.append('file', file);
+        formData.append('id', 'userID test');
+        formData.append('source', 'source test');
 
         setMsg("Uploading...");
         setProgress(prevState => {
             return {...prevState, started: true}
         })
+
         
-        console.log("FORM DATA: ", formData.getAll)
-        const data: DataToSend = {
-            images: formData,
-            id: "test ID",
-            source: "test SOURCE",
-        };
-          
-        // what goes in here??
-        axios.post('http://localhost:8088/blob/', data, {
+        axios.post('http://localhost:8088/blob/', formData, {
             onUploadProgress: ( progressEvent ) => { setProgress(prevState => {
                 if(progressEvent.progress !== undefined) {
                     return {...prevState, prcnt: progressEvent.progress*100}
                 }
                 return {...prevState, prcnt:0}
-            }) },
-            headers: {
-                "Custome-Header": "value"
-            }
+            }) }
         })
         .then(res => {
             setMsg("Uploaded successfully");
-            console.log(res.data)})
+            console.log(res);  // Check the response structure
+            console.log(res.data);})  // Now access res.data
         .catch(err => {
             setMsg("Upload failed");
             console.error(err.response.data)})
