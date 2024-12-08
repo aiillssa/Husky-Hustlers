e784aa74503eb420f844909a02592d977c6cf321
Katharine Zhang <kyiranzhang@gmail.com>
2024-12-07 23:24:47 -0800

    homepage store icon image functionality (unfortunately have not figured out how to change rendering size)

--- frontend/src/pages/Homepage/Homepage.tsx
+++ frontend/src/pages/Homepage/Homepage.tsx
@@ -1,9 +1,12 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import classes from "./Homepage.module.css";
 import { CategoryButton } from "../../components/HomePageC/CategoryButton/CategoryButton";
 import { SearchBar } from "../../components/HomePageC/SearchBar/SearchBar";
 import { getShops } from "../../utils/api";
 import { Link } from "react-router-dom";
+import axios from "axios";
+
+const DEBUG = false;
 
 // Format for contact informatoin
 interface ContactInformation {
@@ -27,6 +30,7 @@ type SellerData = {
   ownerName: string;
   contactInformation: ContactInformation;
   categories: Category[];
+  user: any;
   image?: string;
 };
 
@@ -35,33 +39,85 @@ type HomepageState = {
   listOfSellers: SellerData[];
   selectedType: "food" | "artwork" | "service" | "craft" | "resell" | "all";
   searchTerm: string;
+  imageList: string[];
 };
+
 
 export class Homepage extends Component<{}, HomepageState> {
   constructor(props: {}) {
     super(props);
-    this.state = { listOfSellers: [], selectedType: "all", searchTerm: "" };
+    this.state = { listOfSellers: [], selectedType: "all", searchTerm: "", imageList: [] };
   }
 
   componentDidMount() {
     this.fetchData("all");
   }
 
+
+
   async fetchData(type: string) {
     try {
       const shops = await getShops(type);
+
+      // retrieves all the images saved within the database
+      try {
+        const res = await axios.get('http://localhost:8088/blob/');
+        if (DEBUG) console.log("blob list:", res.data.blobs);
+        this.setImageList(res.data.blobs);  // Assuming 'this' context is correct here
+      } catch (err) {
+        console.error('Error in getting list of blobs', err);
+      }
+      
+
       const sellers = shops.map((shop: SellerData) => {
         const types = shop.categories.map((category) =>
           category.categoryName.toLowerCase()
         ) as ("food" | "artwork" | "service" | "craft" | "resell")[];
-        const placeholderImage = "test";
-        const image = shop.image || placeholderImage;
-        return { ...shop, types };
+        
+        const userID = shop.user.idUsers;
+        if (DEBUG) console.log("current user ID: ", userID)
+        const image = `http://localhost:8088/blob/${Number(userID)}/homepage`
+        
+        let imageExists = false;
+
+        // verifies that the user has an icon image
+        const checkImageExistence = () => {
+          this.state.imageList.forEach((str) => {
+            const [id, source] = str.split('-');
+
+
+            if (DEBUG) console.log("id: ", id);
+            if (DEBUG) console.log("source: ", source);
+            if (DEBUG) console.log("return: ", id === userID.toString() && source === 'homepage');
+
+
+            if(id === userID.toString() && source === 'homepage') {
+              imageExists = true;
+            }
+          })
+        }
+
+        
+        checkImageExistence();
+        console.log(imageExists)
+        if(imageExists) {
+          return { ...shop, types, image };
+        } else {
+          return { ...shop, types};
+        }
+        
+               
       });
       this.setState({ listOfSellers: sellers });
     } catch (err) {
       console.error("Error fetching data:", err);
     }
+  }
+
+
+  setImageList = (list: string[]) => {
+    if (DEBUG) console.log("entered setImageList", list);
+    this.setState({imageList: list});
   }
 
   // add click behavior for each type on homepage
@@ -148,11 +204,21 @@ export class Homepage extends Component<{}, HomepageState> {
             className={classes.cardLink}
           >
               <div className={classes.card}>
-                <img
-                  src={seller.image}
-                  alt={`${seller.shopName} image`}
-                  className={classes.shopImage}
-                />
+
+
+                {/** STORE ICON IMAGE DISPLAY */}
+                {seller.image ? (
+                  <img
+                    src={seller.image}
+                    alt={`${seller.shopName} image`}
+                    width="400px" 
+                    height="400px"
+                  />
+                ) : (
+                  <></>
+                )}
+
+
                 <div className={classes.cardHeading}>
                   <div className={classes.name}>{seller.ownerName}</div>
                   <div className={classes.businessName}>{seller.shopName}</div>
