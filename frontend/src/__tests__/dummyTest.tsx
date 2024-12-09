e04e098b08f38d45b64bb0e91e3a55c5bc1a89c6
Sean0z <seanozinsky@gmail.com>
2024-12-08 22:52:04 -0800

    Added more comments

--- frontend/src/__tests__/dummyTest.tsx
+++ frontend/src/__tests__/dummyTest.tsx
@@ -1,6 +1,4 @@
-// SumComponent.test.js
 import { render, screen, waitFor, within } from '@testing-library/react';
-import DummyComponent from '../components/dummyTestComponent';
 import { getShopWithUserID } from '../utils/api';
 import BusinessPage from '../pages/BusinessPage/BusinessPage';
 import { MemoryRouter } from 'react-router-dom';
@@ -9,7 +7,7 @@ jest.mock('../utils/api', () => ({
   getShopWithUserID: jest.fn(),
 }));
 
-
+// A simple test that shows you the basic framework for testing 
 test('displays new test', async () => {
   const mockUserID = 1;
   const mockShopInfo = { id: 1, name: 'Mock Shop' };
@@ -20,14 +18,17 @@ test('displays new test', async () => {
   expect(result).toEqual(mockShopInfo);
 });
 
+
+// Tests different components of BusinessPage 
 describe('BusinessPage', () => {
+
+  // Clears the mocks before each test and simulates localStorage with a user ID
   beforeEach(() => {
-    // Clear mocks before each test
     jest.clearAllMocks();
-    // Mock localStorage to simulate a user ID
     Storage.prototype.getItem = jest.fn(() => '1');
   });
 
+  // Checks if the user has no shop, then the add my business button is rendered
   it('displays "Add my business" button when the user has no shop', async () => {
     // Mock a 404 response for no shop
     (getShopWithUserID as jest.Mock).mockRejectedValue({
@@ -53,6 +54,7 @@ describe('BusinessPage', () => {
     ).toBeInTheDocument();
   });
 
+  // Checks if the user has a shop, then the shop name and description are rendered
   it('displays the shop information when the user has a shop', async () => {
     // Mock a successful response with shop data
     const mockShopData = {
@@ -82,6 +84,7 @@ describe('BusinessPage', () => {
   
   });
   
+  // Checks if there is an error, then the error message is rendered
   it('displays an error message when the API call fails unexpectedly', async () => {
     // Response when there is an error
     (getShopWithUserID as jest.Mock).mockRejectedValue(new Error('Network Error'));
@@ -99,6 +102,7 @@ describe('BusinessPage', () => {
     });
   });
 
+  // Checks if the user has a shop, then the shop banner is rendered
   it('displays the banner URL when a user has a shop', async () => {
     // Sucess response when user has a shop
     const mockShopData = {
